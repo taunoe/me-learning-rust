fc674dab7d3e0cb84b09c7bf391df5a8dffd41ce
Tauno Erik <sedumacre@gmail.com>
2023-01-14 21:19:12 +0200

    examples

--- kolmas/src/main.rs
+++ kolmas/src/main.rs
@@ -8,5 +8,12 @@ fn main() {
     b += 1;
     println!("a = {} & b = {}", a, b);
 
-    let c: u8 = 256;
+    let c: u8 = 255;
+    println!("c = {}", c);
+
+    let d = 10.1234567890123456789; // f64, 15 kohta peale koma
+    println!("d = {}", d);
+
+    let e: f32 = 10.123456789; // f32, 6 kohta peale koma
+    println!("d = {}", e);
 }
